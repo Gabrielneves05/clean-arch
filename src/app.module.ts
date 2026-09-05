810e356d86958326829b2f570b6c21069dbb21fd
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2026-09-05 11:54:24 -0300

    refactor: env config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    EnvConfigModule,
+    EnvConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,12 +1,30 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 
 import { EnvConfigService } from './env-config.service';
 
+import { join } from 'node:path';
+
 @Module({
-  imports: [ConfigModule],
   providers: [EnvConfigService],
   exports: [EnvConfigService],
 })
-
-export class EnvConfigModule {}
+export class EnvConfigModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    return {
+      module: EnvConfigModule,
+      imports: [
+        ConfigModule.forRoot({
+          ...options,
+          isGlobal: true,
+          envFilePath: [
+            join(
+              __dirname,
+              `../../../../.env.${process.env.NODE_ENV}`,
+            ),
+          ],
+        }),
+      ],
+    };
+  }
+}
