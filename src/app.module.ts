f8baca502965cc7ca4504b36ac52ce1ff4946d1c
Gabriel Neves <129914892+Gabrielneves05@users.noreply.github.com>
2026-09-13 15:06:55 -0300

    refactor: Adding users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
+import { UsersModule } from './users/infrasctructure/users.module';
 
 @Module({
   imports: [
     EnvConfigModule.forRoot(),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
